804fd5344a71e6e5031d9773836fbe10b060a4f1
kebaek <ke.baek@berkeley.edu>
2020-08-12 22:58:04 -0700

    weighted

--- domainbed/algorithms.py
+++ domainbed/algorithms.py
@@ -272,10 +272,10 @@ class MCR(Algorithm):
 			p = torch.from_numpy(p).to(device)
 		elif weighted:
 			for j in range(self.num_classes):
-				svd_j = torch.matmul(torch.eye(self.hparams['fd']).to(device) - torch.matmul(self.components[j].t(),self.components[j]).to(device),x.t().to(device))
-				score_svd_j = torch.norm(svd_j*(1/self.singular_values[j]), dim=0)
+				svd_j = torch.matmul(torch.matmul(self.singular_values[j]*self.components[j].t(),self.components[j]).to(device),x.t().to(device))
+				score_svd_j = torch.norm(svd_j, dim=0)
 				scores_svd.append(score_svd_j)
-			p = torch.argmin(torch.stack(scores_svd), dim=0)
+			p = torch.argmax(torch.stack(scores_svd), dim=0)
         else:
             for j in range(self.num_classes):
 				svd_j = torch.matmul(torch.eye(self.hparams['fd']).to(device) - torch.matmul(self.components[j].t(),self.components[j]).to(device),x.t().to(device))
